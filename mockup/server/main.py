1e452e403efe520b604cd0d7185cfb42fac489c5
Andrea Borghi <andrea.borghi@camptocamp.com>
2022-04-27 15:27:01 +0200

    product_id between 0 - 1000

--- mockup/server/main.py
+++ mockup/server/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {[]}
+    return {"products":"/products/{iri_id}"}
 
 BBOX_CH = ((5.8358140744676303, 45.659168946713827), (10.979311848153316, 47.869910020393519))
 
@@ -21,7 +21,7 @@ def read_item(iri_id: int, q: Optional[str] = None):
     return {
             "id": iri_id,
             "cell_id": '%03d-%02d-%05d-%01d' % (randint(0,999), randint(0,99), randint(0,99999), randint(0,9)) ,
-            "product_id": randint(0,10000000),
+            "product_id": randint(0,1000),
             "iritimestamp": now.strftime(iso_instant_fmt),
             "longitude": random.uniform(BBOX_CH[0][0],BBOX_CH[1][0]),
             "latitude": random.uniform(BBOX_CH[0][1],BBOX_CH[1][1])
